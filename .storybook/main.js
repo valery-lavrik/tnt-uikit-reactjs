ee17c3282d6342d30f4b170555765e6bcc7a3333
lavrik acer <valery.lavrik@gmail.com>
2023-08-21 21:39:45 +0300

    Подключил scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,30 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        {
+            name: "@storybook/addon-styling",
+            options: {
+                sass: {
+                    // Require your preprocessor
+                    implementation: require("sass"),
+                },
+            },
+        },
+    ],
 
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-  typescript: {
-    check: true, // type-check stories during Storybook build
-  },
+    // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+    typescript: {
+        check: true, // type-check stories during Storybook build
+    },
 
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
-  },
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {}
+    },
 
-  docs: {
-    autodocs: true
-  }
+    docs: {
+        autodocs: true
+    }
 };
