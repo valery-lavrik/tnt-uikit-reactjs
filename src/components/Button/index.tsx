79dbe2af1e97a9948738126f7ccdb5e196f29639
Alexander Erkhov <tyrantbud@yandex.ru>
2023-08-25 11:35:10 +0300

    Added table hover and className props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
 interface Props {
     children: React.ReactNode;
     secondary?: boolean;
-    color?: 'blue' | 'red' | 'green' | 'yellow';
+    color?: "blue" | "red" | "green" | "yellow";
     disabled?: boolean;
     onClick?: (e: React.MouseEvent<HTMLElement>) => void;
     style?: React.CSSProperties;
+    className?: string;
 }
 
-const Button = ({ children, secondary = false, color = 'blue', disabled = false, onClick, style = {} }: Props) => {
+const Button = ({
+    children,
+    secondary = false,
+    color = "blue",
+    disabled = false,
+    onClick,
+    style = {},
+    className = "",
+}: Props) => {
     return (
-        <button className={`button ${secondary ? 'button--secondary' : ''} button--${color}`} disabled={disabled} style={style} onClick={onClick}>
+        <button
+            className={`button ${
+                secondary ? "button--secondary" : ""
+            } button--${color} ${className}`}
+            disabled={disabled}
+            style={style}
+            onClick={onClick}
+        >
             {children}
         </button>
     );
